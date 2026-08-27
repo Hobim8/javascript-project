9455471763c40ec2945cb6e1790f2b583a858ab7
Hobim <olaolukomo@gmail.com>
2026-08-27 13:53:46 +0100

    Implement logout functionality with user validation and response handling

--- backend/src/app.js
+++ backend/src/app.js
@@ -10,6 +10,7 @@ app.use(express.json());
 // routes declaration
 app.use("/api/users", userRouter);
 app.use("/api/login", loginUser);
+app.use("/api/logout", loginUser)
 //app.use("/api/v1/posts", postRouter);
 
 
--- backend/src/routes/user.routes.js
+++ backend/src/routes/user.routes.js
@@ -1,11 +1,14 @@
 import { Router } from 'express'
 import { registerUser } from '../controllers/user.controller.js';
 import { loginUser } from '../controllers/user.controller.js';
+import { logoutUser } from '../controllers/user.controller.js';
 const router = Router();
 
 router.post('/register', registerUser);
 
 router.post('/login', loginUser);
+
+router.post('/logout', logoutUser);
 
 
 export default router;
